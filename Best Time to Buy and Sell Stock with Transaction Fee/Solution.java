c9ad802d7418db88ab700a2aaae8fb3f2d66f495
lostplusfound <57611739+lostplusfound@users.noreply.github.com>
2025-06-19 10:50:41 +0800

    Optimized dfs with cache, but it still TLEs

--- Best Time to Buy and Sell Stock with Transaction Fee/Solution.java
+++ Best Time to Buy and Sell Stock with Transaction Fee/Solution.java
@@ -6,6 +6,7 @@ public class Solution {
      * @param fee: a integer
      * @return: return a integer
      */
+    private int[] cache;
     public int maxProfit(int[] prices, int fee) {
         // write your code here
         List<int[]> intervals = new ArrayList<>();
@@ -16,6 +17,7 @@ public class Solution {
                 }
             }
         }
+        cache = new int[intervals.size()];
         int maxProfit = Integer.MIN_VALUE;
         for(int i = 0; i < intervals.size(); i++) {
             int[] initialInterval = intervals.get(i);
@@ -26,16 +28,20 @@ public class Solution {
     }
 
     private int dfs(int[] prices, List<int[]> intervals, int i, int profit, int fee) {
-        int maxProfit = profit;
+        if(cache[i] > 0) {
+            return cache[i];
+        }
+        int maxNextProfit = 0;
         int[] currentInterval = intervals.get(i);
         for(int next = i + 1; next < intervals.size(); next++) {
             int[] nextInterval = intervals.get(next);
             int nextProfit = getProfit(prices, nextInterval[0], nextInterval[1], fee);
             if(nextInterval[0] > currentInterval[1] && nextProfit > 0) {
-                maxProfit = Math.max(maxProfit, dfs(prices, intervals, next, profit + nextProfit, fee));
+                maxNextProfit = Math.max(maxNextProfit, dfs(prices, intervals, next, nextProfit, fee));
             }
         }
-        return maxProfit;
+        cache[i] = maxNextProfit;
+        return profit + maxNextProfit;
     }
 
     private int getProfit(int[] prices, int buyDay, int sellDay, int fee) {
