622dc464e634ccd1411a74278ef23de80df015c5
lostplusfound <57611739+lostplusfound@users.noreply.github.com>
2025-07-19 18:42:02 +0800

    Sort features by popularity passed

---
+++ SortFeaturesByPopularity.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class SortFeaturesByPopularity {
+    /**
+     * @param features: A string array
+     * @param responses: A string array
+     * @return: Array of sorted features
+     */
+    public String[] sortFeatures(String[] features, String[] responses) {
+        // write your code here
+        Map<String, Integer> featureMap = new HashMap<>();
+        for(String f : features) {
+            featureMap.put(f, 0);
+        }
+        for(String r : responses) {
+            Set<String> set = new HashSet<>();
+            String[] words = r.split(" ");
+            for(String w : words) {
+                if(featureMap.containsKey(w)) {
+                    set.add(w);
+                }
+            }
+            for(String f : set) {
+                featureMap.put(f, featureMap.get(f) + 1);
+            }
+        }
+        System.out.println(featureMap);
+        Arrays.sort(features, Comparator.comparing(o -> featureMap.get(o)).reversed());
+        return features;
+    }
+}
