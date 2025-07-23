54ed5cdbf7a158f3cad242d3abf4646c1b54c373
lostplusfound <57611739+lostplusfound@users.noreply.github.com>
2025-07-23 13:42:06 +0800

    Bash game II dfs with memoization accepted

--- BashGameII.java
+++ BashGameII.java
@@ -1,24 +1,38 @@
+import java.util.*;
+
 public class BashGameII {
     /**
      * @param rocks: The number of stones in each pile.
      * @return: Whether you can win the game.
      */
+
     public boolean canWinBash(int[] rocks) {
         // --- write your code here ---
-        return dfs(rocks);
+        return dfs(rocks, new HashMap<>());
     }
 
-    private boolean dfs(int[] rocks) {
+    private boolean dfs(int[] rocks, Map<String, Boolean> memoMap) {
+        String rocksString = "";
+        for(int r : rocks) {
+            if(r > 0) {
+                rocksString += r;
+            }
+        }
+        if(memoMap.containsKey(rocksString)) {
+            return memoMap.get(rocksString);
+        }
         for(int i = 0; i < rocks.length; i++) {
             for(int remove = 1; remove <= rocks[i]; remove++) {
                 rocks[i] -= remove;
-                boolean canWinNext = dfs(rocks);
+                boolean canWinNext = dfs(rocks, memoMap);
                 rocks[i] += remove;
                 if(!canWinNext) {
+                    memoMap.put(rocksString, true);
                     return true;
                 } 
             }
         }
+        memoMap.put(rocksString, false);
         return false;
     }
 }
