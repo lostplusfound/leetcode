82d13fd54d2371a1c6d72cc0bf9eb48fee88342c
lostplusfound <57611739+lostplusfound@users.noreply.github.com>
2025-08-23 14:06:05 -0700

    Removing stars from a string accepted

---
+++ RemovingStars.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class RemovingStars {
+    public String removeStars(String s) {
+        Deque<String> stack = new ArrayDeque<>();
+        for(int i = 0; i < s.length(); i++) {
+            String c = Character.toString(s.charAt(i));
+            if(c.equals("*")) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        String result = "";
+        while(!stack.isEmpty()) {
+            result += stack.removeLast();
+        }
+        return result;
+    }
+}
